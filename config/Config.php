f5a05ed790ab1d7caa19ccaf984010a6d2cba50d
Lorenzuou <59028364+Lorenzuou@users.noreply.github.com>
2019-12-19 11:49:47 -0300

    Update Config.php

--- config/Config.php
+++ config/Config.php
@@ -11,7 +11,7 @@ define('CONTROLLER', 'Home');
 define('METHOD', 'index');
 
 //dados de acesso ao BD
-define('HOST', '35.199.116.219');
+define('HOST', '35.199.109.172');
 define('USER', 'root');
 define('PASS', 'pacote');
 define('DBNAME', 'atelieVirtual');
